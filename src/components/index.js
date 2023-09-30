64d42979fc3387bbde5fa8a2df91dc1fdc4541bf
Gaduh Hartawan <gaduhhartaone@gmail.com>
2023-09-30 16:51:04 +0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import ChannelCard from "./ChannelCard";
+import ChannelDetail from "./ChannelDetail";
+import Feed from "./Feed";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchFeed from "./SearchFeed";
+import Sidebar from "./Sidebar";
+import VideoCard from "./VideoCard";
+import VideoDetail from "./VideoDetail";
+import Videos from "./Videos";
+
+export {
+  Navbar,
+  Feed,
+  VideoDetail,
+  Sidebar,
+  ChannelDetail,
+  SearchFeed,
+  SearchBar,
+  Videos,
+  ChannelCard,
+  VideoCard,
+};
